ec00779218fad61c84532f65d0489c817caf1eda
sr-roboto <marcoslopez123.lu@gmail.com>
2025-03-19 00:54:50 -0300

    Agregar creación de la aplicación Flask y definir rutas en index.py

--- src/index.py
+++ src/index.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template
 
+# Creación de la aplicación Flask
 app = Flask(__name__, static_folder="public/static", template_folder="public/template")
 
+# Rutas de la aplicación
 @app.route("/")
 def home():
     return render_template("index.html")
